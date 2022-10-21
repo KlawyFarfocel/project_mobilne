62a8d0c0c771eb3f31972b35388332eaa43cce0d
KlawyFarfocel <smierciak.maciej@gmail.com>
2022-10-21 15:38:53 +0200

    Modyfikacja MainPage'a - zmiany w strukturze + animacje

--- Gra/Gra/Gra/Gra/MainPage.xaml.cs
+++ Gra/Gra/Gra/Gra/MainPage.xaml.cs
@@ -5,14 +5,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Gra
-{
+{ 
     public partial class MainPage : ContentPage
-    {
+    {    
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            arrow_1 = (Label)FindByName("arrow_1");
+            arrow_2 = (Label)FindByName("arrow_2");
+            arrow_3 = (Label)FindByName("arrow_3");
+            arrow_4 = (Label)FindByName("arrow_4");
+            bigRed = (Button)FindByName("bigRed");
+            animate_pulse(arrow_1);
+            animate_pulse(arrow_2);
+            animate_pulse(arrow_3);
+            animate_pulse(arrow_4);
+            animate_button(bigRed);
+        }
+        void animate_pulse(Label obj)
+        {
+            var Objname = obj.ClassId;
+            var a = new Animation {
+                {0,0.5, new Animation(v => obj.Scale = v, 1, 1.2) },
+                {0.5,1, new Animation(v => obj.Scale = v, 1.2, 1) }               
+            };
+            a.Commit(this,Objname,16,950,Easing.Linear, (v, c) => obj.Scale = 1,()=> true);
+        }
+        void animate_button (Button obj)
+        {
+            var Objname = obj.ClassId;
+            var a = new Animation {
+                {0,0.5, new Animation(v => obj.Opacity = v, 1, 0) },
+                {0.5,1, new Animation(v => obj.Opacity = v, 0, 1) }
+            };
+            a.Commit(this, Objname, 16, 2750, Easing.CubicIn, (v, c) => obj.Opacity = 1, () => true);
         }
     }
 }
