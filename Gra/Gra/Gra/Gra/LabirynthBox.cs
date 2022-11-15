9c65baecdb64dca70916dd14f0f61f8cdec919c7
KlawyFarfocel <smierciak.maciej@gmail.com>
2022-11-15 18:21:28 +0100

    Zagadka labirynt -schemat działania

---
+++ Gra/Gra/Gra/Gra/LabirynthBox.cs
@@ -0,0 +1,70 @@
+﻿using Gra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gra
+{
+    public class LabirynthBox
+    {
+        bool Up;
+        bool Down;
+        bool Left;
+        bool Right;
+        string Id;
+        public LabirynthBox(bool Up, bool Down, bool Left, bool Right, string id)
+        {
+            this.Up = Up;
+            this.Down = Down;
+            this.Left = Left;
+            this.Right = Right;
+            Id = id;
+        }
+        public void CheckCollision(LabirynthBox l, string wall)
+        {
+            if (wall == "Up")
+            {
+                if (l.Up == false)
+                {
+                    //kolizja
+                }
+                else
+                {
+                    //Przejdz w gore
+                }
+            }
+            else if(wall == "Down")
+            {
+                if (l.Down == false)
+                {
+                    //kolizja
+                }
+                else{
+                    //Przejdz w dol
+                }
+            }
+            else if(wall== "Left")
+            {
+                if (l.Left == false)
+                {
+                    //kolizja
+                }
+                else
+                {
+                    //Przejdz w dol
+                }
+            }
+            else if (wall =="Right")
+            {
+                if (l.Right == false)
+                {
+                    //kolizja
+                }
+                else
+                {
+                    //Przejdz w dol
+                }
+            }
+        }
+    }
+}
--- Gra/Gra/Gra/Gra/LabirynthPage.xaml.cs
+++ Gra/Gra/Gra/Gra/LabirynthPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace Gra
 {
@@ -15,6 +17,19 @@ namespace Gra
         public LabirynthPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            for(int i = 0; i < 30; i++)
+            {
+                var blockName = "Kwadrat" + i;
+            }
+            for (var i = 0; i < 30; i++)
+            {
+              //  LabirynthBox name= new LabirynthBox(true, true, true, true, name);
+
+            }
         }
     }
 }
